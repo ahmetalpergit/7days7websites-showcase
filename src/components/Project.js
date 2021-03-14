9571d145f9ad4d80fe1d16cd02e49368d15df037
Ahmet <ahmetalperwork@gmail.com>
2021-03-14 22:23:30 +0300

    final styling before responsive styling is added

--- src/components/Project.js
+++ src/components/Project.js
@@ -11,7 +11,7 @@ function Project({
         <article className="project">
           <div className="project__img-container">
                 <img className="project__img" src={imgPath} alt={"image " + title} />
-                <span className="project__day">{day}</span>
+                <span className="project__day">{'# ' + day}</span>
                 <div className="project__link">
                     <a href={myUrl} className="project__title" target="_blank" rel="noreferrer">{title}</a>
                     <a href={originalUrl} className="project__title" target="_blank" rel="noreferrer">Original</a>
